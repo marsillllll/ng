a5908f48292d8dc379190fea723d29aa1105cba6
marsillllll <ibrahim.halil.marsil@gmail.com>
2022-02-12 14:12:12 +0300

    ng_1

---
+++ start.js
@@ -0,0 +1,28 @@
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const birthday = document.querySelector('#Geburtstag');
+const Gender = document.querySelector('#gender');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  
+  if(nameInput.value === '' || emailInput.value === '' || birthday.value === '' ||  Gender.value === '') {
+    msg.classList.add('error');
+    msg.innerHTML = 'Bitte füllen Sie alle Felder aus';
+    setTimeout(() => msg.remove(), 3000);
+  } 
+else {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(`${nameInput.value} , ${emailInput.value} , ${birthday.value} , ${Gender.value}`));
+    userList.appendChild(li);
+    nameInput.value = '';
+    emailInput.value = '';
+    birthday.value ='';
+    Gender.value = ''; }}
